9dbfe8b23e6493184758702357986d6f5b8cda76
Shu Takayama <syu.takayama@gmail.com>
2024-10-20 03:53:26 +0900

    update

---
+++ macbookpro/.config/nvim/init.lua
@@ -0,0 +1,117 @@
+-- init
+vim.api.nvim_create_augroup("MyVimrc", {})
+
+
+-- providers
+vim.g.python3_host_prog = vim.fn.system("pushd ~ > /dev/null && (asdf which python3 || which python3) 2>/dev/null && popd > /dev/null"):gsub("\n$", "")
+
+
+-- plugins
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazy_path) then
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazy_path })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.runtimepath:prepend(lazy_path)
+
+
+-- edit
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.updatetime = 300
+
+vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
+vim.opt.undofile = true
+
+vim.opt.autowrite = true
+
+vim.opt.confirm = true
+
+vim.opt.shiftwidth = 2
+
+vim.opt.expandtab = true
+
+vim.opt.mouse = "a"
+
+
+-- view
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "»-",
+  trail = "￮",
+  eol = "￩",
+  extends = "»",
+  precedes = "«",
+  nbsp = "￭",
+}
+
+vim.api.nvim_create_autocmd({"VimEnter", "WinEnter"}, {
+  group = "MyVimrc",
+  callback = function()
+    vim.cmd([[match UnicodeSpaces /\%u180E\|\%u2000\|\%u2001\|\%u2002\|\%u2003\|\%u2004\|\%u2005\|\%u2006\|\%u2007\|\%u2008\|\%u2009\|\%u200A\|\%u2028\|\%u2029\|\%u202F\|\%u205F\|\%u3000/]])
+  end,
+})
+vim.api.nvim_create_autocmd({"ColorScheme"}, {
+  group = "MyVimrc",
+  callback = function()
+    vim.cmd([[highlight UnicodeSpaces cterm=underline ctermfg=0 ctermbg=1 gui=underline guifg=#000000 guibg=#800000]])
+  end,
+})
+
+vim.opt.number = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.showmode = false
+
+vim.opt.cursorline = true
+
+vim.opt.showmatch = true
+
+vim.opt.wildmode = "list:longest"
+
+vim.opt.tabstop = 2
+
+vim.opt.conceallevel = 2
+
+vim.g.tex_flavor = "latex"
+vim.g.tex_conceal = "abdmg"
+
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+
+-- LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = "MyVimrc",
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+        end,
+      })
+    end
+  end,
+})
+
+
+-- plugins
+require("lazy").setup({
+  spec = { { import = "plugins" } },
+  checker = { enabled = true },
+  rocks = { enabled = false },
+})
---
+++ macbookpro/.config/nvim/lua/plugins.lua
@@ -0,0 +1,323 @@
+return {
+  -- colorscheme
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("eldritch").setup({
+        transparent = true,
+      })
+      vim.cmd([[colorscheme eldritch]])
+    end,
+  },
+
+  -- libraries
+  {
+    "ryanoasis/vim-devicons",
+    lazy = true,
+    init = function()
+      vim.g.DevIconsEnableFoldersOpenClose = 1
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = true,
+  },
+
+  -- explorer
+  {
+    "preservim/nerdtree",
+    keys = {
+      { "<C-n>", ":NERDTreeToggle<CR>", mode = "n" },
+    },
+    dependencies = {
+      "ryanoasis/vim-devicons",
+    },
+    init = function()
+      vim.api.nvim_create_augroup("MyVimrcNERDTree", {})
+      vim.api.nvim_create_autocmd({"VimEnter", "BufEnter"}, {
+        group = "MyVimrcNERDTree",
+        callback = function()
+          if vim.api.nvim_get_option_value("filetype", {}) == "nerdtree" then
+            vim.cmd([[Fidget suppress true]])
+          else
+            vim.cmd([[Fidget suppress false]])
+          end
+        end,
+      })
+    end,
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      options = {
+        theme = "eldritch",
+      },
+      extensions = {
+        "lazy",
+        "nerdtree",
+        "fugitive",
+      },
+    },
+  },
+
+  -- Tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = "all",
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.clangd.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.ts_ls.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+              languages = {
+                "javascript",
+                "typescript",
+                "vue",
+              },
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      })
+      lspconfig.volar.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.terraformls.setup({})
+      lspconfig.solc.setup({})
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.golangci_lint,
+          require("none-ls.diagnostics.eslint"),
+          require("none-ls.diagnostics.flake8"),
+
+          require("none-ls.formatting.trim_whitespace").with({
+            disabled_filetypes = {
+              "diff",
+            },
+          }),
+          require("none-ls.formatting.trim_newlines").with({
+            disabled_filetypes = {
+              "diff",
+            },
+          }),
+          null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.goimports,
+          require("none-ls.formatting.eslint"),
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.isort,
+          null_ls.builtins.formatting.yapf,
+          null_ls.builtins.formatting.terraform_fmt,
+        },
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls-extras.nvim",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    lazy = false,
+    keys = {
+      { "grci", ":Lspsaga incoming_calls<CR>", mode = "n" },
+      { "grco", ":Lspsaga outgoing_calls<CR>", mode = "n" },
+      { "gra", ":Lspsaga code_action<CR>", mode = "n" },
+      { "grd", ":Lspsaga peek_definition<CR>", mode = "n" },
+      { "grt", ":Lspsaga peek_type_definition<CR>", mode = "n" },
+      { "grr", ":Lspsaga finder<CR>", mode = "n" },
+      { "gri", ":Lspsaga finder imp<CR>", mode = "n" },
+      { "K", ":Lspsaga hover_doc<CR>", mode = "n" },
+      { "grn", ":Lspsaga rename<CR>", mode = "n" },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      finder = {
+        methods = {
+          tyd = "textDocument/typeDefinition",
+        },
+        default = "def+tyd+ref+imp",
+      },
+    },
+  },
+  {
+    "j-hui/fidget.nvim",
+    lazy = false,
+    opts = {},
+  },
+
+  -- GitHub Copilot
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
+
+  -- snippet engine
+  {
+    "hrsh7th/vim-vsnip",
+    keys = {
+      { "<C-j>", function() return vim.fn["vsnip#expandable"]() and "<Plug>(vsnip-expand)" or "<C-j>" end, mode = "i" },
+      { "<C-j>", function() return vim.fn["vsnip#expandable"]() and "<Plug>(vsnip-expand)" or "<C-j>" end, mode = "s" },
+    },
+    dependencies = {
+      "hrsh7th/vim-vsnip-integ",
+    },
+  },
+
+  -- completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-vsnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      local has_space_before = function()
+        local line, column = unpack(vim.api.nvim_win_get_cursor(0))
+        return column == 0 or vim.api.nvim_buf_get_lines(0, line-1, line, true)[1]:sub(column, column):match("%s") ~= nil
+      end
+
+      local feedkeys = function(keys, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, true, true), mode, false)
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if has_space_before() then
+              fallback()
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.fn["vsnip#jumpable"](1) == 1 then
+              feedkeys("<Plug>(vsnip-jump-next)", "")
+            else
+              fallback()
+            end
+          end, {"i", "s"}),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkeys("<Plug>(vsnip-jump-prev)", "")
+            else
+              fallback()
+            end
+          end, {"i", "s"}),
+          ["<C-e>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.close()
+            else
+              fallback()
+            end
+          end, {"i", "s"}),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = true,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-path",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-vsnip",
+    lazy = true,
+    dependencies = {
+      "hrsh7th/vim-vsnip",
+    },
+  },
+
+  -- utils
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  -- language specific
+  {
+    "lervag/vimtex",
+    ft = "tex",
+    init = function()
+      vim.g.vimtex_view_general_viewer = "qpdfview"
+    end,
+  },
+  {
+    "whonore/Coqtail",
+    ft = "coq",
+  },
+}
