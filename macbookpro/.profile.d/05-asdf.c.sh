0e16caf05e38b6f6a2e7853b337c1287b23f3a3d
Shu Takayama <syu.takayama@gmail.com>
2025-02-15 21:21:50 +0900

    update

--- macbookpro/.profile.d/05-asdf.c.sh
+++ macbookpro/.profile.d/05-asdf.c.sh
@@ -1,1 +1,2 @@
-. /opt/asdf-vm/asdf.sh
+export ASDF_DATA_DIR=$HOME/.asdf
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
--- shutakayamanoMacBook-Pro.local/.profile.d/05-asdf.c.sh
+++ shutakayamanoMacBook-Pro.local/.profile.d/05-asdf.c.sh
@@ -1,1 +1,2 @@
-. /opt/homebrew/opt/asdf/libexec/asdf.sh
+export ASDF_DATA_DIR=$HOME/.asdf
+export PATH="$ASDF_DATA_DIR/shims:$PATH"
