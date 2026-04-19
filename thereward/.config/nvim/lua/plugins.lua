a2ac12ed18ddf14850a8c5020bf76e13e51f3e1a
Shu Takayama <syu.takayama@gmail.com>
2026-04-19 20:21:58 +0900

    update

--- thereward/.config/nvim/lua/plugins.lua
+++ thereward/.config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ return {
   -- colorscheme
   {
     "eldritch-theme/eldritch.nvim",
+    branch = "master",
     commit = "0415fa72c348e814a7a6cc9405593a4f812fe12f",
     lazy = false,
     priority = 1000,
@@ -16,6 +17,7 @@ return {
   -- libraries
   {
     "ryanoasis/vim-devicons",
+    branch = "master",
     commit = "71f239af28b7214eebb60d4ea5bd040291fb7e33",
     lazy = true,
     init = function()
@@ -24,11 +26,13 @@ return {
   },
   {
     "nvim-tree/nvim-web-devicons",
+    branch = "master",
     commit = "c72328a5494b4502947a022fe69c0c47e53b6aa6",
     lazy = true,
   },
   {
     "nvim-lua/plenary.nvim",
+    branch = "master",
     commit = "74b06c6c75e4eeb3108ec01852001636d85a932b",
     lazy = true,
   },
@@ -36,6 +40,7 @@ return {
   -- explorer
   {
     "preservim/nerdtree",
+    branch = "master",
     commit = "690d061b591525890f1471c6675bcb5bdc8cdff9",
     cmd = "NERDTree",
     keys = {
@@ -81,6 +86,7 @@ return {
   -- statusline
   {
     "nvim-lualine/lualine.nvim",
+    branch = "master",
     commit = "a905eeebc4e63fdc48b5135d3bf8aea5618fb21c",
     lazy = false,
     dependencies = {
@@ -101,6 +107,7 @@ return {
   -- Tree-sitter
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     commit = "cf12346a3414fa1b06af75c79faebe7f76df080a",
     lazy = false,
     build = ":TSUpdate",
@@ -118,11 +125,13 @@ return {
   -- LSP
   {
     "neovim/nvim-lspconfig",
+    branch = "master",
     commit = "4b7fbaa239c5db6b36f424a4521ca9f1a401be33",
     lazy = false,
   },
   {
     "nvimtools/none-ls.nvim",
+    branch = "main",
     commit = "899e93f9f10251d7220b188eba1b837c0ba27927",
     lazy = false,
     dependencies = {
@@ -163,6 +172,7 @@ return {
   },
   {
     "nvimtools/none-ls-extras.nvim",
+    branch = "main",
     commit = "70ec8815cdf186223af04cc5a15bc8bdface0ef0",
     lazy = true,
     dependencies = {
@@ -171,6 +181,7 @@ return {
   },
   {
     "nvimdev/lspsaga.nvim",
+    branch = "main",
     commit = "562d9724e3869ffd1801c572dd149cc9f8d0cc36",
     lazy = false,
     keys = {
@@ -200,6 +211,7 @@ return {
   },
   {
     "j-hui/fidget.nvim",
+    branch = "main",
     commit = "889e2e96edef4e144965571d46f7a77bcc4d0ddf",
     lazy = false,
     opts = {},
@@ -208,6 +220,7 @@ return {
   -- GitHub Copilot
   {
     "github/copilot.vim",
+    branch = "release",
     commit = "a12fd5672110c8aa7e3c8419e28c96943ca179be",
     lazy = false,
   },
@@ -215,6 +228,7 @@ return {
   -- snippet engine
   {
     "hrsh7th/vim-vsnip",
+    branch = "master",
     commit = "9bcfabea653abdcdac584283b5097c3f8760abaa",
     lazy = false,
     dependencies = {
@@ -223,6 +237,7 @@ return {
   },
   {
     "hrsh7th/vim-vsnip-integ",
+    branch = "master",
     commit = "c7c93934dece8315db3649bdc6898b76358a8b8d",
     lazy = true,
   },
@@ -230,6 +245,7 @@ return {
   -- completion engine
   {
     "hrsh7th/nvim-cmp",
+    branch = "main",
     commit = "a1d504892f2bc56c2e79b65c6faded2fd21f3eca",
     lazy = false,
     dependencies = {
@@ -303,6 +319,7 @@ return {
   },
   {
     "hrsh7th/cmp-nvim-lsp",
+    branch = "main",
     commit = "cbc7b02bb99fae35cb42f514762b89b5126651ef",
     lazy = true,
     dependencies = {
@@ -311,16 +328,19 @@ return {
   },
   {
     "hrsh7th/cmp-buffer",
+    branch = "main",
     commit = "b74fab3656eea9de20a9b8116afa3cfc4ec09657",
     lazy = true,
   },
   {
     "hrsh7th/cmp-path",
+    branch = "main",
     commit = "c642487086dbd9a93160e1679a1327be111cbc25",
     lazy = true,
   },
   {
     "hrsh7th/cmp-vsnip",
+    branch = "main",
     commit = "989a8a73c44e926199bfd05fa7a516d51f2d2752",
     lazy = true,
     dependencies = {
@@ -331,6 +351,7 @@ return {
   -- utils
   {
     "tpope/vim-fugitive",
+    branch = "master",
     commit = "3b753cf8c6a4dcde6edee8827d464ba9b8c4a6f0",
     lazy = false,
   },
@@ -338,6 +359,7 @@ return {
   -- language specific
   {
     "lervag/vimtex",
+    branch = "master",
     commit = "9306903316c3ddd250676b7cf97c84a84c9c8f99",
     ft = "tex",
     init = function()
@@ -347,11 +369,13 @@ return {
   },
   {
     "whonore/Coqtail",
+    branch = "main",
     commit = "e509843fcd41e0bf507283a32fa1316b4fcf92e1",
     ft = "coq",
   },
   {
     'Julian/lean.nvim',
+    branch = "main",
     commit = "6876abd06efb6789081b9f4a3e81503ce07b3c6c",
     ft = "lean",
     dependencies = {
